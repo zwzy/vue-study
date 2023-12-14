05fb4f2ad879fd23fbd149c24ca0826b1329f9a5
zuowang <18755489161@163.com>
2023-12-14 16:22:30 +0800

    feat: ts类型问题

--- src/views/vue/logicReuse/CustomDirective.vue
+++ src/views/vue/logicReuse/CustomDirective.vue
@@ -46,23 +46,23 @@ const vFocus = {
   // 及他自己的所有子节点都挂载完成后调用
   mounted(el: any) {
     el.focus();
-  },
+  }
   // 在绑定元素的 attribute 前
   // 或事件监听器应用前调用
-  created(el, binding, vnode, prevVnode) {
-    // 下面会介绍各个参数的细节
-  },
-  // 在元素被插入到 DOM 前调用
-  beforeMount(el, binding, vnode, prevVnode) {},
-  // 绑定元素的父组件更新前调用
-  beforeUpdate(el, binding, vnode, prevVnode) {},
-  // 在绑定元素的父组件
-  // 及他自己的所有子节点都更新后调用
-  updated(el, binding, vnode, prevVnode) {},
-  // 绑定元素的父组件卸载前调用
-  beforeUnmount(el, binding, vnode, prevVnode) {},
-  // 绑定元素的父组件卸载后调用
-  unmounted(el, binding, vnode, prevVnode) {}
+  // created(el, binding, vnode, prevVnode) {
+  //   // 下面会介绍各个参数的细节
+  // },
+  // // 在元素被插入到 DOM 前调用
+  // beforeMount(el, binding, vnode, prevVnode) {},
+  // // 绑定元素的父组件更新前调用
+  // beforeUpdate(el, binding, vnode, prevVnode) {},
+  // // 在绑定元素的父组件
+  // // 及他自己的所有子节点都更新后调用
+  // updated(el, binding, vnode, prevVnode) {},
+  // // 绑定元素的父组件卸载前调用
+  // beforeUnmount(el, binding, vnode, prevVnode) {},
+  // // 绑定元素的父组件卸载后调用
+  // unmounted(el, binding, vnode, prevVnode) {}
 };
 
 // 简化模式 函数方式 updated 与 mounted 都会调用
